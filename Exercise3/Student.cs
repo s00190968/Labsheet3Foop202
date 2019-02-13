56fec6480e62553eb35f29769c53620fa4070706
Janessa <s00190968@mail.itsligo.ie>
2019-02-13 22:14:32 +0000

    Exercise 3 started

---
+++ Exercise3/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string StudentNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Student(string name, string snumber, string email, string phone)
+        {
+            Name = name;
+            StudentNumber = snumber;
+            Email = email;
+            PhoneNumber = phone;
+        }
+    }
+}
