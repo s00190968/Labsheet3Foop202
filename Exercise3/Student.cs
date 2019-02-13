0444199cf3b87af96c3c804d97ff2a90ecb94468
Janessa <s00190968@mail.itsligo.ie>
2019-02-13 22:59:45 +0000

    Exercise 3 finished.

--- Exercise3/Student.cs
+++ Exercise3/Student.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace Exercise3
 {
-    class Student
+    public class Student
     {
         public string Name { get; set; }
         public string StudentNumber { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
 
-        public Student(string name, string snumber, string email, string phone)
-        {
-            Name = name;
-            StudentNumber = snumber;
-            Email = email;
-            PhoneNumber = phone;
-        }
+        //public Student(string name, string snumber, string email, string phone)
+        //{
+        //    Name = name;
+        //    StudentNumber = snumber;
+        //    Email = email;
+        //    PhoneNumber = phone;
+        //}
     }
 }
